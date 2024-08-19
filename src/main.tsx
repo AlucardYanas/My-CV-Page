7eb5d16bf45ca63a64258e0f0386ff1d5a6143d9
Demetri Bis <26793601+zerthmonk@users.noreply.github.com>
2024-08-19 15:11:50 +0200

    replace: npm -> pnpm, rm: chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
@@ -8,7 +7,5 @@ import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider>
-      <App />
-  </ChakraProvider>,
+  <App />
 );
