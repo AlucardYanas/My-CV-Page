57583413192e0c47318f6e8282dba61ed9dc5382
Artem Pashkov <alucardyanas@gmail.com>
2024-08-19 15:49:06 +0300

    fix: delete Bio, experience, navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import MainPage from './components/pages/MainPage';
-import Bio from './components/ui/Bio';
-import Experience from './components/ui/Experience';
 import Portfolio from './components/ui/Portfolio';
 
 function App(): JSX.Element {
@@ -15,14 +13,6 @@ function App(): JSX.Element {
         {
           path: '/',
           element: <MainPage />,
-        },
-        {
-          path: '/bio',
-          element: <Bio />,
-        },
-        {
-          path: '/experience',
-          element: <Experience />,
         },
         {
           path: 'portfolio',
